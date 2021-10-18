ca2c58456d56026c0295aac1f20238e155ac96e9
Adson Guyllan <adsguyll365@gmail.com>
2021-10-18 12:13:51 -0300

    Operadores aritméticos 1

---
+++ arithmetic_operators.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número: '))
+print('Seu antecessor é {}'.format(n-1), end=' ')
+print('e seu sucessor é {}'.format(n+1))
+print('Seu dobro é {} \n seu triplo é {} \n e sua raiz quadrada é {}'.format(n*2, n*3, n**2))
