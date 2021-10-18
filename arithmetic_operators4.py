5cdede8ebc766d6777120847b861a619fbd854ba
Adson Guyllan <adsguyll365@gmail.com>
2021-10-18 12:34:19 -0300

    Tabuada

---
+++ arithmetic_operators4.py
@@ -0,0 +1,12 @@
+tb = int(input ('Digite um número: '))
+t1 = tb*1
+t2 = tb*2
+t3 = tb*3
+t4 = tb*4
+t5 = tb*5
+t6 = tb*6
+t7 = tb*7
+t8 = tb*8
+t9 = tb*9
+t10 = tb*10
+print ('A tabuada de {} é: \n {} x 1 = {} \n {} x 2 = {} \n {} x 3 = {} \n {} x 4 = {} \n {} x 5 = {} \n {} x6 = {} \n {} x 7 = {} \n {} x 8 = {} \n {} x 9 = {} \n {} x 10 = {}'. format(tb, tb, t1, tb, t2, tb, t3, tb, t4, tb, t5, tb, t6, tb, t7, tb, t8, tb, t9, tb, t10))
