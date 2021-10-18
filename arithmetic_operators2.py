9682cf96672db0e63bacff10f42c2befb078bfa7
Adson Guyllan <adsguyll365@gmail.com>
2021-10-18 12:36:50 -0300

    Cálculos de média das notas

--- arithmetic_operators2.py
+++ arithmetic_operators2.py
@@ -1,5 +1,7 @@
-print(' CALCULO NOTAS')
-n1 = float(input('Digite uma nota '))
-n2 = float(input('Digite outra nota '))
-media = (n1 + n2) / 2
+print('CALCULO NOTAS')
+n1 = float(input('Digite a 1ª nota '))
+n2 = float(input('Digite a 2ª nota '))
+n3 = float(input('Digite a 3ª nota '))
+n4 = float(input('Digite a 4ª nota '))
+media = (n1 + n2 + n3 + n4) / 4
 print('A média é {}'.format(media))
